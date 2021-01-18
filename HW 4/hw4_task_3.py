0ec43a55e66a17855cdd6bbdb989f03526d74a98
vinograd17 <leshaa929@gmail.com>
2021-01-18 23:56:40 +0300

    deleted previous files for HW 4

--- HW 4/hw4_task_3.py
+++
@@ -1,2 +0,0 @@
-# Task 3
-print([el for el in range(20, 241) if el % 20 == 0 or el % 21 == 0])
--- HW 4/hw4_task_5.py
+++
@@ -1,9 +0,0 @@
-# Task 5
-from functools import reduce
-
-
-def my_func(prev_el, el):
-    return prev_el * el
-
-
-print(reduce(my_func, [el for el in range(100, 1001)]))
